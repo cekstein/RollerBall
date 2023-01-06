864892d0bb3bb94f5ede48db798467defe5aad90
cekstein <107091705+cekstein@users.noreply.github.com>
2023-01-05 16:43:23 -0800

    Uploading Unity Project to Github

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class GameCamera : MonoBehaviour
+{
+    public Camera gameCamera;
+
+    public float deltaX;
+    public float deltaY;
+
+    public float xRot;
+    public float yRot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameCamera = Camera.main;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCameraLook(InputValue value)
+    {
+
+    }
+}
